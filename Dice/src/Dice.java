3f74941f0ee08bc7d9e810ccc594fc38cda43af2
Jonathan-Rico <jonyrico231@gmail.com>
2014-04-29 20:28:38 -0700

    Update Dice.java

--- Dice/src/Dice.java
+++ Dice/src/Dice.java
@@ -1,10 +1,11 @@
-public class Dice {
-	public int Throw(int bounces) {
-	int x =	(int) (1 + Math.random() * 6);
-	int average = (x * bounces)/(bounces);
-	return average;
-	}
-	public int value () {
-	return average;	
+public class Dice {	
+	public int Throw(int bounces) {	
+		int x =	(int) (1 + Math.random() * 6);
+		return x;
+		}
+	public int value(int bounces) {
+		int x =	(int) (1 + Math.random() * 6);
+		int average = (x * bounces)/(bounces);
+		return average;	
 	}
 }
