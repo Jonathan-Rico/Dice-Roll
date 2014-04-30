666c3494acf85f1043b8dce5e2aba4ad204fa9b7
Jonathan-Rico <jonyrico231@gmail.com>
2014-04-29 20:00:34 -0700

    Update Main.java

--- Dice/src/Main.java
+++ Dice/src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 public class Main {
 	public static void main(String args []) {
-	Dice dice = new Dice();
-	int x = dice.Throw();
-	int value = dice.value();
-	System.out.printf("The die has rolled a value of %d", value);
+		System.out.printf("Please type in the number of times you want the die to be rolled\n");
+		int bounces = input.nextint();
+		System.out.printf("The die has been rolled %d times", bounces);
+		Dice dice = new Dice();
+		int x = dice.Throw();
+		int value = dice.value();
+		System.out.printf("The die has rolled an average value of %d", value);
 	}
 }
